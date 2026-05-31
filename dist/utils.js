5d3dedae3a768326056a3c08586e9da8eba4707e
TechproTaz <Taz.techzone@gmail.com>
2026-05-31 13:45:29 +0600

    chore: remove dist folder from git

--- dist/utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getRandom = exports.sleep = void 0;
-const sleep = (ms) => new Promise(resovle => setTimeout(resovle, ms));
-exports.sleep = sleep;
-const getRandom = (arr) => arr[Math.floor(Math.random() * arr.length)];
-exports.getRandom = getRandom;
